324576c53d1330c0d9d2322454f2ef4de97d8aec
nepston <maratb2010@mail.ru>
2019-02-22 01:27:21 +0300

    added any views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
   def show
-    @hello = "Салют!"
+    @user = User.new(
+      name: 'Marat',
+      username: 'Nepston',
+      email: 'nepston@gmail.com',
+      avatar_url: 'http://www.picshare.ru/uploads/190221/VS37egkIj9.jpg',
+      password: '321qwe'
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  resource :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'show' => 'users#show'
 end
